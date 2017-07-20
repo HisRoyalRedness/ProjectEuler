fdaf60d588e90aeadc43d8776d3afe16cb0e80bd
Keith Fletcher <keith@fletcher.org>
2017-07-20 23:49:30 +1200

    Further work on roman numerals

--- EulerProblemCommon/Transformers/RomanNumerals.cs
+++ EulerProblemCommon/Transformers/RomanNumerals.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -43,22 +44,74 @@ namespace HisRoyalRedness.com
             iv.   C can only be placed before D and M.
         */
 
-        public static ulong FromRomanNumerals(this string number, bool throwIfInvalid = true)
+        public static ulong FromRomanNumerals(this string number, bool throwIfInvalid = true, bool ignoreSpace = false)
         {
             // M CM D CD C XC L XL X IX IV V I
             var tokens1 = "MCMDCDCXCLXLXIXIVVI".RomanScanner().ToList();
             var tokens2 = "M CM D CD C XC L XL X IX IV V I".RomanScanner(true).ToList();
 
+            var state = RomanTokenType.None;
+            var sum = (ulong)0;
+            foreach (var token in RomanScanner(number, ignoreSpace))
+            {
+                if (token.TokenType == RomanTokenType.EOS)
+                    break;
 
-            return 0;
+                switch(token.TokenType)
+                {
+                    #region M
+                    case RomanTokenType.M:
+                        if (state <= RomanTokenType.M)
+                            state = token.TokenType;
+                        else
+                            ParseError(number, token.Index);
+                        break;
+                    #endregion M
+                    #region CM
+                    case RomanTokenType.CM:
+                        if (state <= RomanTokenType.M)
+                            state = token.TokenType;
+                        else
+                            ParseError(number, token.Index);
+                        break;
+                    #endregion CM
+                    #region D
+                    case RomanTokenType.D:
+                        if (state <= RomanTokenType.D)
+                            state = token.TokenType;
+                        else
+                            ParseError(number, token.Index);
+                        break;
+                    #endregion D
+                    #region CD
+                    case RomanTokenType.CD:
+                        if (state <= RomanTokenType.D)
+                            state = token.TokenType;
+                        else
+                            ParseError(number, token.Index);
+                        break;
+                    #endregion CD
+                    default:
+                        ParseError(number, token.Index);
+                        return 0;
+                }
+
+                sum += _tokenValues[token.TokenType];
+            }
+
+            return sum;
         }
 
+        static void ParseError(string number, int index)
+        {
+            throw new InvalidOperationException($"{number} is not a valid Roman numberal. The digit '{number[index - 1]}' at position {index} is unexpected.");
+        }
 
         static IEnumerable<RomanToken> RomanScanner(this string number, bool ignoreSpace = false)
         {
             if ((number?.Length ?? 0) == 0)
             {
-                yield return RomanToken.EOS;
+                yield return new RomanToken(RomanTokenType.EOS, 0);
                 yield break;
             }
 
@@ -67,73 +120,74 @@ namespace HisRoyalRedness.com
             var i = 1;
             var fetchNext = false;
 
-            while(true)
+            while (true)
             {
-                while(true)
+                while (true)
                 {
                     currentChar = nextChar;
-                    nextChar = (i >= number.Length) ? '\0' : char.ToUpper(number[i++]);
+                    nextChar = (i >= number.Length) ? '\0' : char.ToUpper(number[i]);
+                    i++;
                     if (fetchNext)
                         fetchNext = false;
                     else
                         break;
-                } 
+                }
 
-                switch(currentChar)
+                switch (currentChar)
                 {
-                    case 'M': yield return RomanToken.M; break;
-                    case 'D': yield return RomanToken.D; break;
+                    case 'M': yield return new RomanToken(RomanTokenType.M, i); break;
+                    case 'D': yield return new RomanToken(RomanTokenType.D, i); break;
                     case 'C':
                         if (nextChar == 'M')
                         {
                             fetchNext = true;
-                            yield return RomanToken.CM;
+                            yield return new RomanToken(RomanTokenType.CM, i);
                         }
                         else if (nextChar == 'D')
                         {
                             fetchNext = true;
-                            yield return RomanToken.CD;
+                            yield return new RomanToken(RomanTokenType.CD, i);
                         }
                         else
-                            yield return RomanToken.C;
+                            yield return new RomanToken(RomanTokenType.C, i);
                         break;
-                    case 'L': yield return RomanToken.L; break;
+                    case 'L': yield return new RomanToken(RomanTokenType.L, i); break;
                     case 'X':
                         if (nextChar == 'L')
                         {
                             fetchNext = true;
-                            yield return RomanToken.XL;
+                            yield return new RomanToken(RomanTokenType.XL, i);
                         }
                         else if (nextChar == 'C')
                         {
                             fetchNext = true;
-                            yield return RomanToken.XC;
+                            yield return new RomanToken(RomanTokenType.XC, i);
                         }
                         else
-                            yield return RomanToken.X;
+                            yield return new RomanToken(RomanTokenType.X, i);
                         break;
-                    case 'V': yield return RomanToken.V; break;
+                    case 'V': yield return new RomanToken(RomanTokenType.V, i); break;
                     case 'I':
                         if (nextChar == 'V')
                         {
                             fetchNext = true;
-                            yield return RomanToken.IV;
+                            yield return new RomanToken(RomanTokenType.IV, i);
                         }
                         else if (nextChar == 'X')
                         {
                             fetchNext = true;
-                            yield return RomanToken.IX;
+                            yield return new RomanToken(RomanTokenType.IX, i);
                         }
                         else
-                            yield return RomanToken.I;
+                            yield return new RomanToken(RomanTokenType.I, i);
                         break;
                     case '\0':
-                        yield return RomanToken.EOS;
+                        yield return new RomanToken(RomanTokenType.EOS, i);
                         yield break;
                     default:
                         if (!ignoreSpace || currentChar != ' ')
                         {
-                            yield return RomanToken.Invalid;
+                            yield return new RomanToken(RomanTokenType.Invalid, i);
                             yield break;
                         }
                         break;
@@ -141,15 +195,23 @@ namespace HisRoyalRedness.com
             }
         }
 
-        enum RomanToken { M, D, C, L, X, V, I, CD, CM, XL, XC, IV, IX, Invalid, EOS };
-
-
-        static void ParseError(string number, int index)
+        [DebuggerDisplay("{_type}: {_index}")]
+        struct RomanToken
         {
-            throw new InvalidOperationException($"{number} is not a valid Roman numberal. The digit '{number[index - 1]}' at position {index} is unexpected.");
+            internal RomanToken(RomanTokenType tokenType, int index)
+            {
+                _type = tokenType;
+                _index = index - 1;
+            }
+
+            public RomanTokenType TokenType => _type;
+            public int Index => _index;
+
+            readonly RomanTokenType _type;
+            readonly int _index;
         }
 
-        
+        enum RomanTokenType { None, M, CM, D, CD, C, XC, L, XL, X, IX, V, IV, I, Invalid, EOS };
 
         #region ToRomanNumerals
         public static string ToRomanNumerals(this ulong number, bool subtractive = true)
@@ -184,7 +246,14 @@ namespace HisRoyalRedness.com
         const int ROMAN_I = 1;
         const int ROMAN__ = 0;
 
-        static Tuple<char, ulong, char, ulong>[] _romanDenominations = new[]
+        const int ROMAN_CM = ROMAN_M - ROMAN_C;
+        const int ROMAN_CD = ROMAN_D - ROMAN_C;
+        const int ROMAN_XC = ROMAN_C - ROMAN_X;
+        const int ROMAN_XL = ROMAN_L - ROMAN_X;
+        const int ROMAN_IX = ROMAN_X - ROMAN_I;
+        const int ROMAN_IV = ROMAN_V - ROMAN_I;
+
+        readonly static Tuple<char, ulong, char, ulong>[] _romanDenominations = new[]
         {
             new Tuple<char, ulong, char, ulong>('M', ROMAN_M, 'C', ROMAN_C),
             new Tuple<char, ulong, char, ulong>('D', ROMAN_D, 'C', ROMAN_C),
@@ -193,6 +262,23 @@ namespace HisRoyalRedness.com
             new Tuple<char, ulong, char, ulong>('X', ROMAN_X, 'I', ROMAN_I),
             new Tuple<char, ulong, char, ulong>('V', ROMAN_V, 'I', ROMAN_I),
             new Tuple<char, ulong, char, ulong>('I', ROMAN_I, '\0', ROMAN__),
+        };
+
+        readonly static Dictionary<RomanTokenType, ulong> _tokenValues = new Dictionary<RomanTokenType, ulong>()
+        {
+            { RomanTokenType.M, ROMAN_M },
+            { RomanTokenType.CM, ROMAN_CM },
+            { RomanTokenType.D, ROMAN_D },
+            { RomanTokenType.CD, ROMAN_CD },
+            { RomanTokenType.C, ROMAN_C },
+            { RomanTokenType.XC, ROMAN_XC },
+            { RomanTokenType.L, ROMAN_L },
+            { RomanTokenType.XL, ROMAN_XL },
+            { RomanTokenType.X, ROMAN_X },
+            { RomanTokenType.IX, ROMAN_IX },
+            { RomanTokenType.V, ROMAN_V },
+            { RomanTokenType.IV, ROMAN_IV },
+            { RomanTokenType.I, ROMAN_I },
         };
     }
 }
